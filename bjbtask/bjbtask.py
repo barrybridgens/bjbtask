d9de111cbb4ce825cdf66a3ff34f6fac8f3c8ec3
Barry Bridgens <barry@bridgens.me.uk>
2013-12-23 15:05:03 +0000

    Initial parsing

--- bjbtask/bjbtask.py
+++ bjbtask/bjbtask.py
@@ -1,12 +1,29 @@
+#!/usr/bin/python
 #
 # bjbtask - Barry's command line task manager
 #
 
-import argparse
+import sys
 
-parser = argparse.ArgumentParser(description='Command line task manager.')
-parser.add_argument('add')
+class bjbTask:
 
-args = parser.parse_args()
+    def __init__(self):
+        pass
 
-print (args)
+    def arg_parse(self, argv):
+        if argv[1] == 'add':
+            self.add(argv)
+        elif argv[1] == 'show':
+            self.show(argv)
+        else:
+            print ("Command not recognised: {}".format(argv[1]))
+
+    def add(self, argv):
+        print ("---- Add command")
+
+    def show(self, argv):
+        print ("---- Show Command")
+
+if __name__ == "__main__":
+    bjb_task = bjbTask()
+    bjb_task.arg_parse(sys.argv)
