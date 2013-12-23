4f2421cb13e5959c44604938a620759ab5a01129
Barry Bridgens <barry@bridgens.me.uk>
2013-12-23 16:28:37 +0000

    Delete functionality added

--- bjbtask/bjbtask.py
+++ bjbtask/bjbtask.py
@@ -22,6 +22,8 @@ class bjbTask:
             self.add(argv[2:])
         elif argv[1] == 'show':
             self.show(argv[2:])
+        elif ((argv[1] == 'del') or (argv[1] == 'delete')):
+            self.delete(argv[2:])
         else:
             print ("Command not recognised: {}".format(argv[1]))
         self.save()
@@ -38,6 +40,19 @@ class bjbTask:
         for task in bjbTask.tasks:
             print ("{}   {}".format((num + 1), task))
             num = num + 1
+
+    def delete(self, argv):
+        try:
+            num = int(argv[0])
+        except exceptions.ValueError:
+            print ("Error: Task number not valid")
+            num = len(bjbTask.tasks) + 1
+        if ((num > 0) and ((num <= len(bjbTask.tasks)))):
+            text = bjbTask.tasks[num - 1]
+            del(bjbTask.tasks[num - 1])
+            print ("Task deleted: {}".format(text))
+        else:
+            print ("Task mumber out of range: {}".format(num))
 
     def save(self):
         # Overwrite file with all current data - THIS WILL NOT SCALE!!!!
